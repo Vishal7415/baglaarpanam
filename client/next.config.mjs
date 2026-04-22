eead2d5537c22327f0997d6c3af8eeb148e0999a
Vishal7415 <vishal7415@example.com>
2026-04-23 00:36:47 +0530

    chore: add next.config updates and .env.example

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbopack: {
-      root: './'
-    }
-  }
+  output: 'export',
 };
 
 export default nextConfig;
